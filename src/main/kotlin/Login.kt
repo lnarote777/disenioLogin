4999f736def8de2eeb41e831d2e99645c0fd7b9e
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-04-24 15:57:09 +0200

    terminado, falta ponerlo en el centro

--- src/main/kotlin/Login.kt
+++ src/main/kotlin/Login.kt
@@ -1,15 +1,19 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.sp
 import javax.swing.text.PasswordView
 
@@ -17,35 +21,44 @@ import javax.swing.text.PasswordView
 @Preview
 fun Login() {
 
-    MaterialTheme{
-            var name by remember { mutableStateOf("") }
-
+    MaterialTheme {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            var usuario by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            var viewPassword by remember { mutableStateOf(false) }
 
             OutlinedTextField(
-                value = name,
-                onValueChange = {name = it},
+                value = usuario,
+                onValueChange = { usuario = it },
                 label = { Text("Usuario") },
             )
 
-
-    }
-}
-
-@Composable
-@Preview
-fun PasswordView(){
-    var password by remember { mutableStateOf("") }
-
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth(),
-        value = password,
-        onValueChange = {password = it},
-        placeholder = {
-            Text(
-                text = "Contraseña"
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Contraseña") },
+                visualTransformation = if (viewPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconToggleButton(
+                        checked = viewPassword,
+                        onCheckedChange = {viewPassword = it}
+                    ){
+                        Icon(
+                            imageVector = if (viewPassword) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                            contentDescription = "View"
+                        )
+                    }
+                }
             )
-        }
-    ){}
 
+            Button(
+                onClick = {usuario = ""; password = ""},
+            ){
+                Text("Login")
+            }
+        }
+    }
 }
